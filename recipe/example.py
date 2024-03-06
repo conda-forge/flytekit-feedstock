51501cdf7ceddaed9cb742c7080f8ba57795ae90
Bernhard Stadlbauer <11799671+bstadlbauer@users.noreply.github.com>
2024-03-06 15:38:49 +0100

    Change example to not require pandas

--- recipe/example.py
+++ recipe/example.py
@@ -1,17 +1,12 @@
-import typing
-import pandas as pd
-import numpy as np
-
 from flytekit import task, workflow
 
-@task
-def generate_normal_df(n:int, mean: float, sigma: float) -> pd.DataFrame:
-    return pd.DataFrame({"numbers": np.random.normal(mean, sigma,size=n)})
 
 @task
-def compute_stats(df: pd.DataFrame) -> typing.Tuple[float, float]:
-    return float(df["numbers"].mean()), float(df["numbers"].std())
+def say_hello(name: str) -> str:
+    return f"Hello, {name}!"
+
 
 @workflow
-def wf(n: int = 200, mean: float = 0.0, sigma: float = 1.0) -> typing.Tuple[float, float]:
-    return compute_stats(df=generate_normal_df(n=n, mean=mean, sigma=sigma))
+def hello_world_wf(name: str = 'world') -> str:
+    res = say_hello(name=name)
+    return res
